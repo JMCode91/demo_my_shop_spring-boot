29849e9fc63acd2db903553471a3b658fb88777c
Morenfer <jmorenfer@gmail.com>
2026-01-29 13:11:48 +0100

    añadimos controladores y plantillas html

---
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+
+public class WebController {
+
+    @GetMapping("/")
+    @PostMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+
+    @RequestMapping("/checkout")
+    public String checkout(Model model) {
+        return "checkout";
+    }
+}
