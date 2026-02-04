9ccbdd2ca757bbfda30627aad9d5cc694b0153bd
Morenfer <jmorenfer@gmail.com>
2026-02-04 18:20:49 +0100

    añadimos excepciones para el formulario de registro de usuario

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,14 +3,17 @@ package com.example.demo.controller;
 import com.example.demo.domain.User;
 import com.example.demo.exception.UserRegistrationException;
 import com.example.demo.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -29,7 +32,7 @@ public class UserController {
 
     // Procesa el formulario (POST)
     @PostMapping("/new-user")
-    public String addUser(@ModelAttribute User user, Model model) {
+    public String addUser(@ModelAttribute User user, Model model) throws UserRegistrationException {
         logger.info("inicio addUser");
         boolean userAdded = userService.add(user);
 
@@ -37,9 +40,41 @@ public class UserController {
             throw new UserRegistrationException("Error al registrar el usuario");
         }
 
-        logger.info("Usuario creado: " + user);
+        logger.info("Usuario creado: {}", user);
         model.addAttribute("user", user);
         logger.info("final addUser");
         return "new-user";
     }
+
+
+
+
+    @ExceptionHandler(UserRegistrationException.class)
+    public ModelAndView handleUserRegistrationException(HttpServletRequest request, UserRegistrationException exception) {
+
+        ModelAndView mav = new ModelAndView();
+
+        // Aquí definimos el mensaje que saldrá en la pantalla
+        mav.addObject("message", "No se ha podido registrar el usuario. Por favor contacte con soporte técnico");
+        mav.addObject("exception", exception);
+        mav.addObject("url", request.getRequestURL());
+
+        // Aquí le decimos que cargue la página "error.html" (que crearemos luego)
+        mav.setViewName("error");
+
+        return mav;
+    }
+
+    @ExceptionHandler
+    public ModelAndView handleException(HttpServletRequest request, Exception exception) {
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("message", exception.getMessage());
+        mav.addObject("exception", exception);
+        mav.addObject("url", request.getRequestURL());
+        mav.setViewName("error");
+        return mav;
+    }
+
+
 }
